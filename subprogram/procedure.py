4434bd1c7d9b1520cb6753a986950b5264fd9dad
yusufwdn <yusuf.wandana1@gmail.com>
2024-04-02 14:31:00 +0700

    procedure (function without return)

---
+++ subprogram/procedure.py
@@ -0,0 +1,16 @@
+# Berbeda dengan fungsi, prosedur tidak mengharuskan adanya parameter input atau output dan dapat dipandang 
+# sebagai fungsi yang tidak menghasilkan nilai. Dalam Python, prosedur didefinisikan dengan return tanpa ekspresi 
+# atau nilai yang dihasilkan di akhir fungsi.
+
+def greeting(name):
+    print("Halo " + name + ", bagaimana kabarmu?")
+
+def greeting_new(name):
+    print("Halo " + name + ", bagaimana kabarmu?")
+
+greeting_new("Cupskyyy")
+
+"""
+Output: 
+Halo Cupskyyy, bagaimana kabarmu?
+"""
